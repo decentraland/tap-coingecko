b87b6d5429e8624e35c14c57d42a1888b47c85f3
Manuel Ponsa <manuponsa@gmail.com>
2023-04-07 18:07:21 -0300

    Create tap-coingecko for multiple tokens"

---
+++ tap_coingecko/tap.py
@@ -0,0 +1,56 @@
+"""Coingecko tap class."""
+
+from importlib.metadata import requires
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+from tap_coingecko.streams import (
+    CoingeckoStream,
+)
+
+STREAM_TYPES = [
+    CoingeckoStream
+]
+
+
+class TapCoingecko(Tap):
+    """Coingecko tap class."""
+    name = "tap-coingecko"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "tokens",
+            th.StringType,
+            required=True,
+            description="The name of the tokens to import the price history of separated by commas",
+            default="ethereum"
+        ),
+        th.Property(
+            "api_url",
+            th.StringType,
+            required=True,
+            description="Coingecko's api url",
+            default="https://api.coingecko.com/api/v3"
+        ),
+        th.Property(
+            "start_date",
+            th.StringType,
+            required=True,
+            description="First date to obtain token data price for, this will be used for all tokens in the list and may result in empty price data for some tokens",
+            default="2022-03-01"
+        ),
+        th.Property(
+            "wait_time_between_requests",
+            th.IntegerType,
+            required=True,
+            description="Number of seconds to wait between requests",
+            default=5
+        )
+    ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
